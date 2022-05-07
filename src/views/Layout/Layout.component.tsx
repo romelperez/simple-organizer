b0806814d18e965658856ede2eaa37911beec7b6
Romel Perez <ronelprhone@gmail.com>
2022-05-06 19:42:23 -0500

    fix: header

--- src/views/Layout/Layout.component.tsx
+++ src/views/Layout/Layout.component.tsx
@@ -4,7 +4,12 @@ import { Link, Outlet } from 'react-router-dom';
 const Layout = (): ReactElement => {
   return (
     <div>
-      <header>
+      <header
+        style={{
+          marginBottom: 20,
+          borderBottom: '1px solid black'
+        }}
+      >
         <Link to='/'>
           <h1>Simple Organizer</h1>
         </Link>
